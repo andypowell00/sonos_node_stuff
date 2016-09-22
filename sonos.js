a7d062b32cad1ba0303ab65eb956ea849a4e9720
andypowell00 <apuk45@gmail.com>
2016-09-22 16:55:36 -0500

    quick fix

--- sonos.js
+++ sonos.js
@@ -4,51 +4,50 @@ var util = require('util');
 var Listener = require('./node_modules/sonos/lib/events/listener.js')
 var _sonos = new Sonos(process.env.SONOS_HOST || '10.20.0.62')
 var x = new Listener(_sonos);
-var isConnected = false;
-
 
 //start socket listening
 
 var app = require('express')();
-var server = app.listen(80);
-var io = require('socket.io')(app);
-var fs = require('fs');
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+
+server.listen(80);
 
 app.get('/', function (req, res) {
-    res.sendfile( __dirname + '/index.html' );
+    res.sendFile( __dirname + '/index.html' );
 });
 
 
 io.on('connection', function (socket) {
   startSonosListener(socket);
   console.log('client connected');
-  isConnected = true;
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
+  
+
   socket.on('disconnect', function () {
-    io.emit('user disconnected');
-    isConnected = false;
+    console.log('user disconnected');
+    
   });
 });
+
 //connect to sonos and emit when event occurs
 function startSonosListener(socket){
+
 x.listen(function(err) {
   if (err) throw err;
   
   x.addService('/MediaRenderer/AVTransport/Event', function(error, sid) {
     if (error) throw err;
     console.log('Successfully subscribed, with subscription id', sid);
+    
   });
 
   x.on('serviceEvent', function(endpoint, sid, data) {
     console.log('Received event from', endpoint, '(' + sid + ') with data:', data, '\n\n');
 
  _sonos.currentTrack(function(err, track) {
-        console.log('--track below--')
-        //console.log(err, track);
-        socket.emit('newtrack', track.title);
-        console.log(track.title); });
+        socket.emit('newtrack', track);
+        });
 
  
  });
