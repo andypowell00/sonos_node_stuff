9ab1495b0ac4086ec2568ea1b0f7cae9930b9a28
andypowell00 <apuk45@gmail.com>
2016-09-26 10:50:07 -0500

    updates

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
 
+
 server.listen(5337,'0.0.0.0');
 
 app.get('/', function (req, res) {
@@ -12,15 +13,19 @@ app.get('/', function (req, res) {
 
 
 io.on('connection', function (socket) {
-  socket.join('sonosroom'); //throw all connections into same room
-  console.log('client connected');  
+    socket.join('sonosroom'); //throw all connections into same room
+    console.log('client connected ');  
   
   socket.on('disconnect', function () {
-    console.log('user disconnected');   
-    
+    console.log('user disconnected ');   
+  });
+  socket.on('veto',function(){
+    _sonos.veto();
   });
   socket.on('listen', function() {
+      console.log('check = ' + _sonos.checkListener());
+    if(_sonos.checkListener() === false){
     console.log('start listening....');
-    _sonos.startlistening(io);
+    _sonos.startlistening(io);}else{_sonos.newConnect(io);}
   });
 });
--- sonos.js
+++ sonos.js
@@ -4,6 +4,8 @@ var util = require('util');
 var Listener = require('./node_modules/sonos/lib/events/listener.js')
 var _sonos = new Sonos(process.env.SONOS_HOST || '10.20.0.62')
 var x = new Listener(_sonos);
+var isListening = false;
+var sidholder = "";
 
 
 //connect to sonos and emit when event occurs
@@ -19,7 +21,8 @@ x.listen(function(err) {
   });
   x.on('serviceEvent', function(endpoint, sid, data) {
     console.log('Received event from', endpoint, '(' + sid + ') with data:', data, '\n\n');
-
+    isListening = true;
+    sidholder = sid;
   currTrack(io);
  });
 });
@@ -29,7 +32,28 @@ x.listen(function(err) {
 }
 ,veto:  function(){
 
+      /*_sonos.next(function (err, nexted) {
+        if (!err || !nexted) {
+          console.log('Complete');
+        } else {
+          console.log('errrrrrrrrrrrrrr');
+        }});*/
+        _sonos.play('http://www.freesfx.co.uk/rx2/mp3s/5/16800_1460741032.mp3', function (err, playing) {
+          console.log([err, playing])
+        });
 
+}
+,checkListener: function(){
+  
+  return isListening;
+},
+killListener: function(){
+  x.removeService(sidholder,function(err){
+  if(err) throw err;
+  console.log('service killed');
+  isListening = false;
+  });
+  
 }
 
 }
@@ -39,7 +63,6 @@ function currTrack(io){
         if(err) throw err;
         io.to('sonosroom').emit('newtrack', track); //broadcast changes to the room
         });
-
 }
 
 
